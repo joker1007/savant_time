fed357f5dbeb656b254b6d02b53f8870475e0422
joker1007 <kakyoin.hierophant@gmail.com>
2013-05-16 02:39:19 +0900

    Fix worker args

--- app/controllers/archive_jobs_controller.rb
+++ app/controllers/archive_jobs_controller.rb
@@ -2,7 +2,7 @@ class ArchiveJobsController < ApplicationController
   def create
     root_path = Pathname.new(Settings.root_dir)
     real_path = root_path + "#{current_path}.#{params[:format]}"
-    ArchiveWorker.perform_async(real_path)
+    ArchiveWorker.perform_async(real_path.to_s)
     render nothing: true, status: :ok
   end
 end
