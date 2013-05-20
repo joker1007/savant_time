08e321e4620309812d2195b04398ff3b7a3254bc
joker1007 <kakyoin.hierophant@gmail.com>
2013-05-21 02:20:48 +0900

    update rails

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SavantTime::Application.config.session_store :encrypted_cookie_store, key: '_savant_time_session'
+SavantTime::Application.config.session_store :cookie_store, key: '_savant_time_session'
