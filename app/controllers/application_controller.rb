a78fb851c57b10bf0d3e70846b1ad875327bccd9
joker1007 <kakyoin.hierophant@gmail.com>
2013-06-16 13:42:40 +0900

    Implement HTTP Digest auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :digest_authenticate
+
   protected
   def current_path
     params[:path] || ""
   end
   helper_method :current_path
+
+  private
+  def digest_authenticate
+    authenticate_or_request_with_http_digest do |username|
+      if username == Settings.login_user
+        Settings.login_password
+      else
+        false
+      end
+    end
+  end
 end
