dbf82411387e93b1293011301e366238367f45f7
joker1007 <kakyoin.hierophant@gmail.com>
2014-04-02 19:03:30 +0900

    Remove decorators spec

--- spec/decorators/file_entry_decorator_spec.rb
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-require 'spec_helper'
-
-describe FileEntryDecorator do
-  let(:file_entry) { FileEntry.new.extend FileEntryDecorator }
-  subject { file_entry }
-  it { should be_a FileEntry }
-end
